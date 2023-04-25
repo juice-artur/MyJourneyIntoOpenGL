3c7277fff9d14292d2c03e847630b0572fb144d1
Artur Dzhus <asasin.lol10@gmail.com>
2023-04-25 22:39:45 +0300

    [REFACTORING] Added resize window callback

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,68 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
+
+void FramebufferSizeCallback(GLFWwindow* Window, int Width, int Height);
+void ProcessInput(GLFWwindow* window);
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
 
 int main(void)
 {
     GLFWwindow* window;
 
-    if (!glfwInit()) return -1;
+    if (!glfwInit())
+    {
+        return -1;
+    }
 
-    window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
+
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
 
-    if (!gladLoadGL())
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Can't load GLAD!" << std::endl;
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
     }
-
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
-
-    glClearColor(0.5, 1, 0.3, 1);
 
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
+        ProcessInput(window);
 
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void FramebufferSizeCallback(GLFWwindow* Window, int Width, int Height)
+{
+    glViewport(0, 0, Width, Height);
+}
+
+void ProcessInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }

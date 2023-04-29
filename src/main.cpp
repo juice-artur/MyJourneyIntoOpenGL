f8cb9f381976ee536d0266627169431e19b5ae1b
Artur Dzhus <asasin.lol10@gmail.com>
2023-04-29 16:15:23 +0300

    Added cube to the scene

--- src/main.cpp
+++ src/main.cpp
@@ -45,29 +45,73 @@ int main(void)
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, 1.0f, 1.0f,    //
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,   // 
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,  // 
-        -0.5f, 0.5f, 0.0f, 0.0f, 1.0f    // 
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  //
+        0.5f, -0.5f, -0.5f, 1.0f, 0.0f,   //
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,    //
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,    //
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,   //
+        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,  //
+                                          //
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,   //
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,    //
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,     //
+        0.5f, 0.5f, 0.5f, 1.0f, 1.0f,     //
+        -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,    //
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,   //
+                                          //
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,    //
+        -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,   //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,   //
+        -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,    //
+                                          //
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,     //
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,    //
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,   //
+        0.5f, -0.5f, -0.5f, 0.0f, 1.0f,   //
+        0.5f, -0.5f, 0.5f, 0.0f, 0.0f,    //
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,     //
+                                          //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+        0.5f, -0.5f, -0.5f, 1.0f, 1.0f,   //
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,    //
+        0.5f, -0.5f, 0.5f, 1.0f, 0.0f,    //
+        -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,   //
+        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,  //
+                                          //
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,   //
+        0.5f, 0.5f, -0.5f, 1.0f, 1.0f,    //
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,     //
+        0.5f, 0.5f, 0.5f, 1.0f, 0.0f,     //
+        -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,    //
+        -0.5f, 0.5f, -0.5f, 0.0f, 1.0f    //
     };
 
-    unsigned int indices[] = {
-        0, 1, 3,  //
-        1, 2, 3   //
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),      //
+        glm::vec3(2.0f, 5.0f, -15.0f),    //
+        glm::vec3(-1.5f, -2.2f, -2.5f),   //
+        glm::vec3(-3.8f, -2.0f, -12.3f),  //
+        glm::vec3(2.4f, -0.4f, -3.5f),    //
+        glm::vec3(-1.7f, 3.0f, -7.5f),    //
+        glm::vec3(1.3f, -2.0f, -2.5f),    //
+        glm::vec3(1.5f, 2.0f, -2.5f),     //
+        glm::vec3(1.5f, 0.2f, -1.5f),     //
+        glm::vec3(-1.3f, 1.0f, -1.5f)     //
     };
 
 
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
     glGenVertexArrays(1, &VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glBindVertexArray(VAO);
 
@@ -76,7 +120,7 @@ int main(void)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
     unsigned int texture1;
@@ -127,20 +171,29 @@ int main(void)
     glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
     ourShader.SetInt("texture2", 1);
 
-
     while (!glfwWindowShouldClose(window))
     {
         ProcessInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glm::mat4 transform = glm::mat4(1.0f); 
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        glm::mat4 transform = glm::mat4(1.0f);
         transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
         transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
@@ -148,8 +201,27 @@ int main(void)
         unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+
+        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        unsigned int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(VAO);
+
+        for (auto cubePosition : cubePositions)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePosition);
+
+            float angle = 20.0f;
+            model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+       
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -157,7 +229,6 @@ int main(void)
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
